4c790d8b7f5718389e5f8a50fdb32d4f48f1822a
chichuakeso <kesariachichua@gmail.com>
2024-11-11 23:17:36 +0400

    18

---
+++ Day 18/classwork/cw.py
@@ -0,0 +1,9 @@
+# მომხმარებელს შეეკითხეთ სახელი სანამ ეს სახელი არიქნება გაბრიელი მაქამდე არ გაჩერდეს და შეეკითხოს ისევ ხელახლა თუ იქნება გაბრიელი გაჩერდეს
+
+
+name = input("enter your name: ")
+name1 = "kesaria"
+while name != name1:
+    name = input("enter your name: ")
+if name == name1:
+     print("approved")
