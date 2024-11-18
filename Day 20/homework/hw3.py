916a7b717bd6d3699f6869b222cc6d50f96baf89
chichuakeso <kesariachichua@gmail.com>
2024-11-18 11:02:33 +0400

    20

--- Day 20/homework/hw3.py
+++ Day 20/homework/hw3.py
@@ -2,5 +2,5 @@
 
 alnst = ["sua", "ivan", "till", "mizi", "hyuna", "luka", "hyunwoo", "acron", "silas", "orian", "unknown", "romh", "alnst", "nari", "blinkgone"]
 
-for i in range(1, len(alnst), 2): #ყოველ მეორე ინდექსზე გადავა
+for i in range(2, len(alnst), 2): #ყოველ მეორე ინდექსზე გადავა
     print(alnst[i])
--- Day 20/homework/hw4.py
+++ Day 20/homework/hw4.py
@@ -1,8 +1,8 @@
 #  შექმენით დიდი სია სადაც გექნებათ ბევრი ელემენტი და შემდეგ გამოიტანთ 5 ის ჯერად ინდექსებზე მდგომ ელემენტებს
 
-nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty"]
+nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty"]
 
-i = 4
+i = 5
 while i < len(nums):
     print(nums[i])
     i += 5
