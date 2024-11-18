14523ce523b6611337e28c98ce18885a76a275da
chichuakeso <kesariachichua@gmail.com>
2024-11-18 11:59:18 +0400

    22

---
+++ Day 22/classwork/cw.py
@@ -0,0 +1,9 @@
+# მომხაარებელ შემოატანინეთ სახელი და გვარი ეს სახელი და გვარი გააერთიანეთ და შეაუბრუნეთ
+
+
+name = input("enter your name: ")
+surename = input("enter your surename: ")
+full_name = (name + " " + surename)
+print(full_name)
+print(full_name[::-1])
+
