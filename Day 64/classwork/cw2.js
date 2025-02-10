a602e7c8ab1765fb4bd990ea28a9a40702d82420
chichuakeso <kesariachichua@gmail.com>
2025-02-11 01:53:18 +0400

    commit

---
+++ Day 64/classwork/cw2.js
@@ -0,0 +1,14 @@
+var firstName = prompt("enter name: ")
+var lastName = prompt ("enter surename: ")
+
+console.log("Hello " + firstName + " " + lastName)
+
+var num1 = prompt("enter a number: ")
+var num2 = prompt("enter another numbere: ")
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+
+alert("welcome "  + firstName + " " + lastName)
