57950f714c12492ad476b1ad3e4dc4471b9af5b9
chichuakeso <kesariachichua@gmail.com>
2025-04-05 00:50:09 +0400

    commit

---
+++ Day 84/classwork/cw1.js
@@ -0,0 +1,6 @@
+/*HTML-ში შექმენით რამდენიმე ელემენტი , შემდეგ  javasCript-იდან გაუწერეთ ისეთი ლოგიკა რომ,რომელ ელემენტსაც დააკლიკებთ ,ამ ელემენტის ტექსტის ფერი გახდეს წითელი ასევე კონსოლში დაბეჭდოს ამ კონკრეტული ელემენტის ინფორმაცია(აუცილებლად გამოიყენეთ დღეს ახსნილი event object)*/
+
+document.addEventListener('click', function(event){
+    console.log(event.target)
+    event.target.style.color = 'red'
+})
---
+++ Day 84/classwork/cw2.js
@@ -0,0 +1,8 @@
+/*შექმენით 4 div-კონტეინერი,თითოეულ დივს მიეცით id , თუ რა ფერი უნდა გახდეს ეს დივი დაკლიკების შემდეგ,ასევე ამ დივში უნდა იყოს პარაგრაფი(Click to see color), შემდეგ javaascript-იდან შეამოწმეთ რომელ DIV-ზე განხორციელდა მოვლენა , ასევე შეამოწმეთ ამ DIV- ის ID-ები და მიანიჭეთ შესაბამისი ფერი ,  ასევე შეცვალეთ paragraph-ის textContent იმ ფერის სახელით რა ფერითაც შეიცვლება დივი */
+
+document.addEventListener('click', function(event){
+    
+    console.log(event.target.id)
+    event.target.textContent = event.target.id
+    event.target.style.backgroundColor = event.target.id
+})
